57f595f026a036f4881e9a97a55f5ce7c3fff561
khency_h <khency.hiciano@gmail.com>
2024-01-22 19:33:50 -0500

    wip: Changes button text when clicked

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ var cat = document.querySelector(".cat");
 button.addEventListener("click", function () {
   if (cat.classList.contains("show")) {
     cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
   } else {
     cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
   }
 });
