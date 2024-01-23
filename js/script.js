ff64a9ff6eb215273e8be64b1b3ea9692727739d
khency_h <khency.hiciano@gmail.com>
2024-01-22 19:26:07 -0500

    wip: Adds functionality to button to make image disappear when clicked

--- js/script.js
+++ js/script.js
@@ -3,5 +3,9 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 
 button.addEventListener("click", function () {
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+  } else {
+    cat.classList.add("show");
+  }
 });
